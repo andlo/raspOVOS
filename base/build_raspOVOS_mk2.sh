0d5a8ddb7cdfe25490f9ef087b2c4d42733f67ea
andlo <andlo@outlook.dk>
2024-12-29 00:59:21 +0100

    ...

--- base/build_raspOVOS_mk2.sh
+++ base/build_raspOVOS_mk2.sh
@@ -11,24 +11,24 @@ cp -v /mounted-github-repo/patches/boot_config.txt /boot/firmware/config.txt
 
 # Mark II devices
 
-# cp -v /mounted-github-repo/mark2/sj201-daemon.conf "/etc/pulse/daemon.conf.d/sj201-daemon.conf"
+# cp -v /mounted-github-repo/mark2/files/sj201-daemon.conf "/etc/pulse/daemon.conf.d/sj201-daemon.conf"
 # chmod 0644 "/etc/pulse/daemon.conf.d/sj201-daemon.conf"
-# cp -v /mounted-github-repo/mark2/92-sj201-default.pa "/etc/pulse/default.pa.d/92-sj201-default.pa"
+# cp -v /mounted-github-repo/mark2/files/92-sj201-default.pa "/etc/pulse/default.pa.d/92-sj201-default.pa"
 # chmod 0644 "/etc/pulse/default.pa.d/92-sj201-default.pa"
-cp -v /mounted-github-repo/mark2/91-vocalfusion.rules "/etc/udev/rules.d/91-vocalfusion.rules"
+cp -v /mounted-github-repo/mark2/files/91-vocalfusion.rules "/etc/udev/rules.d/91-vocalfusion.rules"
 chmod 0644 "/etc/udev/rules.d/91-vocalfusion.rules"
-cp -v /mounted-github-repo/mark2/xvf3510.conf "/usr/share/pulseaudio/alsa-mixer/profile-sets/xvf3510.conf"
+cp -v /mounted-github-repo/mark2/files/xvf3510.conf "/usr/share/pulseaudio/alsa-mixer/profile-sets/xvf3510.conf"
 chmod 0644 "/usr/share/pulseaudio/alsa-mixer/profile-sets/xvf3510.conf"
-cp -v /mounted-github-repo/mark2/xvf3510-flash "/usr/libexec/xvf3510-flash"
+cp -v /mounted-github-repo/mark2/files/xvf3510-flash "/usr/libexec/xvf3510-flash"
 chmod 0755 "/usr/libexec/xvf3510-flash"
-# cp -v /mounted-github-repo/mark2/xvf3510.dtbo "/boot/firmware/overlays/xvf3510.dtbo"
+# cp -v /mounted-github-repo/mark2/files/xvf3510.dtbo "/boot/firmware/overlays/xvf3510.dtbo"
 chmod 0755 "/usr/lib/firmware"
 chmod 0755 "/usr/lib/firmware/xvf3510"
-cp -v /mounted-github-repo/mark2/app_xvf3510_int_spi_boot_v4_2_0.bin "/usr/lib/firmware/xvf3510/app_xvf3510_int_spi_boot_v4_2_0.bin"
+cp -v /mounted-github-repo/mark2/files/app_xvf3510_int_spi_boot_v4_2_0.bin "/usr/lib/firmware/xvf3510/app_xvf3510_int_spi_boot_v4_2_0.bin"
 chmod 0644 "/usr/lib/firmware/xvf3510/app_xvf3510_int_spi_boot_v4_2_0.bin"
-cp -v /mounted-github-repo/mark2/sj201-reset-led "/usr/bin/sj201-reset-led"
+cp -v /mounted-github-repo/mark2/files/sj201-reset-led "/usr/bin/sj201-reset-led"
 chmod 0755 "/usr/bin/sj201-reset-led"
-cp -v /mounted-github-repo/mark2/tas5806-init "/usr/bin/tas5806-init"
+cp -v /mounted-github-repo/mark2/files/tas5806-init "/usr/bin/tas5806-init"
 chmod 0755 "/usr/bin/tas5806-init"
 
 
